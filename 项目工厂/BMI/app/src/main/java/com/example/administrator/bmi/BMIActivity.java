f1442cbf22eb17708a8d73aa37bffe8cccecde40
CMQ19940709 <CLX19940601@163.com>
2015-07-13 20:21:35 +0800

    项目工厂

---
+++ 项目工厂/BMI/app/src/main/java/com/example/administrator/bmi/BMIActivity.java
@@ -0,0 +1,86 @@
+package com.example.administrator.bmi;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class BMIActivity extends Activity {
+    private EditText height = null;
+    private EditText weight = null;
+    private Button button = null;
+    private TextView show = null;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bmi);
+
+        Button button=(Button)findViewById(R.id.tton);
+
+
+        height=(EditText) findViewById(R.id.height);
+        weight=(EditText) findViewById(R.id.weight);
+        show=(TextView) findViewById(R.id.show);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            double bmi=0;
+
+            public void onClick(View v) {
+
+                double h=new Double(height.getText().toString());
+                double w=new Double(weight.getText().toString());
+                bmi=w/(h*h/10000);
+
+                double final_resault = exchangeResault(bmi);
+                if(bmi<18.5) {
+
+                    show.setText("体质指数：" + final_resault + "  " + "偏轻");
+                }
+
+                else if(bmi>=18.5||bmi<24){
+
+                    show.setText("体质指数："+final_resault+"  "+"正常");
+                }
+
+                else if(bmi>=24||bmi<27){
+
+                    show.setText("体质指数："+final_resault+"  "+"偏重");
+                }
+
+                else if(bmi>=27||bmi<30){
+
+                    show.setText("体质指数："+final_resault+"  "+"轻度肥胖");
+                }
+
+                else if(bmi>=30||bmi<35){
+
+                    show.setText("体质指数："+final_resault+"  "+"中度肥胖");
+                }
+
+                else if(bmi>35){
+
+                    show.setText("体质指数："+final_resault+"  "+"重度肥胖");
+                }
+            }
+        });
+    }
+
+    public double exchangeResault(double resault){
+        double first =resault*100;
+        double second = (int) first;
+
+        double second_first = first-second;
+
+        if(second_first>0.5){
+            second++;
+        }
+
+        double jirguo = (double)second/100;
+        return jirguo;
+    }
+
+}
